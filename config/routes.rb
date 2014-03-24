ae1331db7eebaacac671dd45aa21115d2a540e25
Tobias Rupp <tobias.rupp@gmx.com>
2014-03-24 11:39:33 +0000

    Added route for version 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Bandshowcase::Application.routes.draw do
 
   root to: "public_pages#version_2"
+
+  get "version_1" => "public_pages#version_1"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
