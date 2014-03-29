8ef36b5a39488ae706d373ff7f480f54ede72735
Tobias Rupp <tobias.rupp@gmx.com>
2014-03-29 20:10:31 +0000

    Changes Facebook Sharer path

--- app/helpers/public_pages_helper.rb
+++ app/helpers/public_pages_helper.rb
@@ -3,6 +3,6 @@ module PublicPagesHelper
 
   def facebook_sharer_url(title, path)
     # "https://www.facebook.com/sharer.php?s=100 &p[url]=#{request.protocol + request.host_with_port + '/' + path}&p[images][0]=#{asset_url('vogel_144_b.png')}&p[title]=#{title}&p[summary]=#{@fb_description}"
-    "https://www.facebook.com/dialog/feed?app_id=1430206933893155 &display=popup&caption=#{title} &link=#{request.protocol + request.host_with_port + '/' + path}&redirect_uri=#{request.protocol + request.host_with_port + request.fullpath}"
+    "https://www.facebook.com/dialog/feed?app_id=1430206933893155&display=popup&caption=#{title}&link=#{request.protocol + request.host_with_port + '/' + path}&redirect_uri=#{request.protocol + request.host_with_port + request.fullpath}"
   end
 end
