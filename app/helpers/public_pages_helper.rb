ad0842ca8f5e625fcfa8bb404cb52be0a7c3f766
Tobias Rupp <tobias.rupp@gmx.com>
2014-03-29 23:01:31 +0000

    Facebook Share is now in separate window

--- app/helpers/public_pages_helper.rb
+++ app/helpers/public_pages_helper.rb
@@ -2,6 +2,6 @@ module PublicPagesHelper
 
 
   def facebook_sharer_url(title, path, picture=asset_url('vogel_144_b.png'))
-    "https://www.facebook.com/dialog/feed?app_id=1430206933893155&name=#{title}&link=#{request.protocol + request.host_with_port + '/' + path}&redirect_uri=#{request.protocol + request.host_with_port + request.fullpath}&picture=#{asset_url(picture)}"
+    "https://www.facebook.com/dialog/feed?app_id=1430206933893155&name=#{title}&link=#{request.protocol + request.host_with_port + '/' + path}&redirect_uri=#{root_url}&picture=#{asset_url(picture)}"
   end
 end
