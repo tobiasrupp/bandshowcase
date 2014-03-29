1d1afab5d0cddb082237f1bfef590f390ec6fd2d
Tobias Rupp <tobias.rupp@gmx.com>
2014-03-29 19:28:54 +0000

    Added custom parameters to Facebook Sharer urls

--- app/helpers/public_pages_helper.rb
+++ app/helpers/public_pages_helper.rb
@@ -1,2 +1,7 @@
 module PublicPagesHelper
+
+
+  def facebook_sharer_url(title, path)
+    "https://www.facebook.com/sharer.php?s=100 &p[url]=#{request.protocol + request.host_with_port + path}&p[images][0]=#{asset_url('vogel_144_b.png')}&p[title]=#{title}&p[summary]=#{@fb_description}"
+  end
 end
