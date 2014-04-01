0cb6d3dbf7aa90761e0100c83273aadd3a8a0cfd
Tobias Rupp <tobias.rupp@gmx.com>
2014-04-01 20:09:34 +0100

    Changed share_callback url

--- app/helpers/public_pages_helper.rb
+++ app/helpers/public_pages_helper.rb
@@ -3,6 +3,6 @@ module PublicPagesHelper
 
   def facebook_sharer_url(title, path, picture=asset_url('vogel_144_b.png'))
     url = request.protocol + request.host_with_port + '/' + path
-    "https://www.facebook.com/dialog/feed?app_id=1430206933893155&name=#{title}&link=#{url}&redirect_uri=#{share_callback_url + '/' + url}&picture=#{asset_url(picture)}"
+    "https://www.facebook.com/dialog/feed?app_id=1430206933893155&name=#{title}&link=#{url}&redirect_uri=#{share_callback_url + '?url=' + url}&picture=#{asset_url(picture)}"
   end
 end
